36425defc5f4cb8d4f1186d385e22d822ac67645
Christian Bell <christiantcbell@gmail.com>
2019-07-18 12:34:38 +0100

    working with associated functions (a bit like class methods) and methods (like instance methods)

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,37 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    println!("Rectangle is {:#?}", rect1);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    fn square(size: u32) -> Rectangle {
+        Rectangle { height: size, width: size }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 30 };
+    let rect3 = Rectangle { width: 60, height: 100 };
+
+    println!("Rectangle 1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle 1 is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(20);
+
+    println!("Square is {:#?}", sq);
+
 }
